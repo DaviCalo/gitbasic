42ea52706fc44ef61263194d05adec4f996b15f2
Davi <davicalo2d@gmail.com>
2024-06-13 11:10:19 -0300

    extrutura

--- app/src/main/java/com/example/basicgit/MainActivity.kt
+++ app/src/main/java/com/example/basicgit/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.basicgit.ui.theme.BasicgitTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,4 +59,47 @@ fun GreetingPreview() {
     BasicgitTheme {
         Greeting("Android")
     }
+}
+
+@Composable
+fun screen(){
+    Column(
+        Modifier
+            .fillMaxSize()
+            .navigationBarsPadding()
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .height(80.dp)
+                .background(Color.Black),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(text = "Boa noite, Davi", color = Color.White, fontSize = 25.sp)
+        }
+        Column {
+            Row(
+                modifier = Modifier
+                    .padding(25.dp)
+                    .width(200.dp)
+                    .height(120.dp)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(Color.Cyan),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(text = "Box Davi", fontSize = 25.sp)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun asd(){
+    scren()
 }
